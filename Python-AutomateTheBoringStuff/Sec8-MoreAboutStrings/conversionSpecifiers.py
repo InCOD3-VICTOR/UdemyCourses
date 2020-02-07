39156e9ff65e8b29dda2d3e03722b368e0db06b7
Kayti <kaytipotgieter@gmail.com>
2020-02-06 16:30:19 -0800

    added a file to Sec8 with conversion specifiers

---
+++ Python-AutomateTheBoringStuff/Sec8-MoreAboutStrings/conversionSpecifiers.py
@@ -0,0 +1,12 @@
+#Using Conversion Specifiers
+name = 'Sam'
+place = 'Down town Bart Station'
+time = '3am'
+food = 'Chips and Salsa' 
+
+print('Hello %s, you are invited to a party at %s at %s. Please bring %s.'%(name, place, time, food))
+
+
+
+
+#converions specifiers help make code more readable 
