e7eb3a46de45302edba8d5dbd8d5b89177e317f9
Kayti <kaytipotgieter@gmail.com>
2020-02-10 10:10:05 -0800

    updated reg expressions example

--- Python-AutomateTheBoringStuff/Sec10-RegularExpressions/example.py
+++ Python-AutomateTheBoringStuff/Sec10-RegularExpressions/example.py
@@ -1,17 +1,19 @@
 def isPhoneNumber(text): #415-555-5555
     if len(text) !=12:
         return False #not phone number sized
-    for i in range(0,3):
-        if not text(i).isdecimal():
+    for i in range(0, 3):
+        if not text[i].isdecimal():
             return False #no area code
-        if text(3)!= '-':
+    if text[3] != '-':
             return False #missing dash
-        for i in range(4,7):
-            if not text(i).isdecimal():
+    for i in range(4, 7):
+            if not text[i].isdecimal():
                 return Flase #no first 3 digits
-        if text(7) != '-':
+    if text[7] != '-':
             return False #missing second dash
-        for in in range(8,12):
-            if not text(i).isdecimal():
-                return Fals 
-            
+    for i in range(8, 12):
+            if not text[i].isdecimal():
+                return False #missing last 4 digits
+    return True 
+
+print(isPhoneNumber('510-550-0555'))
